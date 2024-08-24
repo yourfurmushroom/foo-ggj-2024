134d24c0349c21365dddf84514d6ab161dd0174c
s7887177 <58907092+s7887177@users.noreply.github.com>
2024-08-24 17:46:27 +0800

    c

---
+++ Assets/Eason/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public class GameOverWindow : MonoBehaviour
+{
+
+}
+
+public class GameMenuWindow : MonoBehaviour
+{
+
+}
+
+public class MainMenuWindow : MonoBehaviour
+{
+
+}
+
+public class GamePlayWindow : MonoBehaviour
+{
+
+}
