b92c35f4701f4012b30dd1076720bfaaddffa5ea
yourfurmushroom <Zihui_0411@hotmail.com>
2024-08-25 00:54:03 +0800

    refactor background controller

--- Assets/zihui/script/BackgroundController.cs
+++ Assets/zihui/script/BackgroundController.cs
@@ -1,45 +1,91 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BackgroundController : MonoBehaviour
 {
-    public float scrollSpeed = 0.1f;
-    public float totalHeight;  
-    public List<Transform> backgrounds; 
+    public float _speed = 0.1f;
+    [SerializeField] private Transform[] _backgrounds; 
 
     private Vector3 startPosition;
+    private int _count;
     
-
+    private float _startTime;
+    private float[] _heights;
+    private float[] _startPosY;
     private void Awake()
     {
-        for (int i = 0; i < this.transform.childCount; i++)
+
+        _count = this.transform.childCount;
+        _backgrounds = new Transform[_count];
+        _heights = new float[_count];
+        _startPosY = new float[_count];
+
+        for (int i = 0; i < _count; i++)
         {
-            backgrounds.Add(this.transform.GetChild(i));
+            _backgrounds[i] = this.transform.GetChild(i).transform;
         }
+        UpdateHeightAndPosY();
     }
     void Start()
     {
-        startPosition = backgrounds[0].position;
-        totalHeight = Math.Abs(backgrounds[1].position.y)* this.transform.childCount;
-        Debug.Log(totalHeight);
+
+        Debug.Log(17.56 % 8);
+    }
+    public void FixedUpdate()
+    {
+        OnUpdate(Time.time);
     }
 
-    void Update()
+    public void Initialize(float startTime)
     {
-        for (int i = 0; i < backgrounds.Count; i++)
+        _startTime = startTime;
+    }
+    public void OnUpdate(float time)
+    {
+        var timeElapse = time - _startTime;
+        var totalHeight = _heights.Sum();
+        Debug.Log(totalHeight);
+        timeElapse %= (totalHeight / _speed);
+        for (int i = 0; i < _backgrounds.Length; i++)
         {
-            backgrounds[i].Translate(Vector2.up * scrollSpeed * Time.deltaTime);
-
-            if (backgrounds[i].position.y > startPosition.y + totalHeight/ this.transform.childCount)
-            {
-                Vector3 newPos = backgrounds[i].position;
-                newPos.y -= totalHeight;
-                backgrounds[i].position = newPos;
-            }
+            _backgrounds[i].position =  Vector2.up * (_startPosY[i] + _speed * timeElapse);
+        }
+        if (_backgrounds[_count-1].position.y > 0)
+        {
+            _backgrounds[0].position -= totalHeight * Vector3.up;
         }
     }
-
+    //private void Rotate()
+    //{
+    //    var bg = _backgrounds[0];
+    //    for (int i = 0; i < _count-1; i++)
+    //    {
+    //        _backgrounds[i] = _backgrounds[i + 1];
+    //    }
+    //    _backgrounds[_count - 1] = bg;
+    //    UpdateHeightAndPosY();
+    //}
+    private void UpdateHeightAndPosY()
+    {
+        for (int i = 0; i < _count; i++)
+        {
+            var sprite = _backgrounds[i].GetComponent<SpriteRenderer>().sprite;
+            _heights[i] = sprite.texture.height / sprite.pixelsPerUnit;
+        }
+        for (int i = 1; i < _count; i++)
+        {
+           
+            Debug.Log($"{i}: {_heights[i]}");
+            _startPosY[i] =
+                _startPosY[i - 1] -
+                _heights[i - 1] / 2 -
+                _heights[i] / 2;
+        }
+    }
 }
 
